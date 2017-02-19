38b48b6b423b20838eb7ad8be57d9721e45f13e8
Adarshkumar Pavani <adarshkumar.pavani@gmail.com>
2017-02-18 17:25:57 -0800

    smoothed out the character movements upon tap

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -3,27 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterController : MonoBehaviour {
-	Animator anim;
-	float upMag, sideMag;
+    Animator anim;
+    float upMag, sideMag;
     bool one_click = false;
     float timer_for_double_click;
     //this is how long in seconds to allow for a double click
     float delay = 0.25f;
     int taps = 0;
-
+    private bool isTransitioning;
+    private MovementData mData = new MovementData();
     // Use this for initialization
-    void Start () {
-		upMag = 200f;
-		sideMag = 400f;
+    void Start() {
+        upMag = 2f;
+        sideMag = 4f;
 
-		anim = GetComponent<Animator> ();
-	}
+        anim = GetComponent<Animator>();
+    }
 
     // Update is called once per frame
     void Update()
     {
 
-//        if (isTouchingPlatform)
+        //        if (isTouchingPlatform)
         {
             if (Input.GetMouseButtonDown(0))
             {
@@ -38,7 +39,7 @@ public class CharacterController : MonoBehaviour {
 
                     //do double click stuff here
                     taps = 2;
-                    singleOrDouble(taps);                    
+                    singleOrDouble(taps);
                     Debug.Log("Double Click");
                 }
             }
@@ -62,20 +63,61 @@ public class CharacterController : MonoBehaviour {
 
     void singleOrDouble(int tapCount)
     {
-        if (tapCount > 0)
-		{ int direction=1;
-			if (Input.mousePosition.x > Screen.width / 2)
-			    direction = 1; //to indicate right;
-				anim.SetInteger("State",1);	
-			
-			if (Input.mousePosition.x < Screen.width / 2)
-			    direction = -1; //to indicate left
+        if (isTransitioning) //previously triggered co routine is still in progress
+            return;
+        mData.tapCount = tapCount;
 
-            Camera.main.transform.Translate(Vector3.up * Time.deltaTime * upMag);
-            transform.Translate(Vector3.up *  Time.deltaTime * upMag);
-            transform.Translate(Vector3.right * direction * Time.deltaTime * sideMag * tapCount);
-            taps = 0;
+        if (tapCount > 0)
+        {
+            if (Input.mousePosition.x > Screen.width / 2)
+            {
+                mData.target = new Vector3(transform.position.x+5*tapCount, transform.position.y+4);
+                StopCoroutine("MoveNext");
+                StartCoroutine("MoveNext");
+            }
+            if (Input.mousePosition.x < Screen.width / 2)
+            {
+                mData.target = new Vector3(transform.position.x-5*tapCount, transform.position.y+4);
+                StopCoroutine("MoveNext");
+                StartCoroutine("MoveNext");
+            }
+
+            //Camera.main.transform.Translate(Vector3.up * Time.deltaTime * upMag);
         }
     }
-    
+
+    private IEnumerator MoveNext()
+    {
+        this.isTransitioning = true;
+        //first finish animation
+        anim.SetInteger("State", 1);
+        while (anim.GetAnimatorTransitionInfo(0).IsName("left"))
+            yield return null;
+
+        //set animation back to idle
+        anim.SetInteger("State", 1);
+
+        //move the character
+        float startTime = Time.time;
+        float journeyLength = Vector3.Distance(transform.position, mData.target);
+        Vector3 startPosition = transform.position;
+        Vector3 cameraStart = Camera.main.transform.position;
+        float speed = 15f;
+        while (transform.position.y < mData.target.y)
+        {
+            float distCovered = (Time.time - startTime) * speed;
+            float fracJourney = distCovered / journeyLength;
+            transform.position = Vector3.Lerp(startPosition, mData.target, fracJourney);
+            float deltaCharacter = transform.position.y - startPosition.y;
+            Camera.main.transform.position = new Vector3(cameraStart.x, cameraStart.y+deltaCharacter, cameraStart.z);
+            yield return null;
+        }
+        this.isTransitioning = false;
+    }
+}
+
+public class MovementData
+{
+    public Vector3 target;
+    public int tapCount;
 }
